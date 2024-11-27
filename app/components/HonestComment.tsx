37124b879029b43559bf3aac1c90d7f4dd9c2c85
Alexandre Saussier <asaussier99@gmail.com>
2024-11-27 16:33:46 +0000

    fixes

--- app/components/HonestComment.tsx
+++ app/components/HonestComment.tsx
@@ -5,7 +5,7 @@ interface HonestCommentProps {
   export default function HonestComment({ comment }: HonestCommentProps) {
     return (
       <div className="mt-4 p-4 bg-gray-100 rounded-md relative">
-        <h4 className="text-xs font-semibold text-gray-500 absolute top-2 left-2">Honest comment from me</h4>
+        <h4 className="text-xs font-semibold text-gray-500 absolute top-2 left-2">Honest Comment</h4>
         <p className="text-sm italic text-gray-700 mt-4">
           &quot;{comment}&quot;
         </p>
