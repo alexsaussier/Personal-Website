a0d68a91679d1e432c2bc1f764d24258b5ecad94
Alexandre Saussier <asaussier99@gmail.com>
2024-12-02 14:49:58 +0000

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    
     <html lang="en" className="scroll-smooth">
+      <script defer src="https://cloud.umami.is/script.js" data-website-id="f56265fd-def1-42b9-aa9c-30b981af9d3b"></script>
       <body className={`${inter.className} bg-gray-50 text-gray-900`}>{children}</body>
     </html>
   )
